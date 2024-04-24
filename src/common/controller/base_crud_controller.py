e492fec628a66273ddb8441c2fade7f3c6dbd09e
Ruan Gustavo Moretti <ruan.gustavo.moretti10@gmail.com>
2024-04-24 19:35:13 -0300

    feat: new version of base crud controller

--- src/common/controller/base_crud_controller.py
+++ src/common/controller/base_crud_controller.py
@@ -1,12 +1,23 @@
 from abc import abstractmethod
 
 from common.controller.base_controller import BaseController
+from common.controller.base_list_controller import BaseListController
 from common.gui.widget.base_crud_widget import BaseCRUDWidget
 
 
 class BaseCRUDController(BaseController):
     _widget: BaseCRUDWidget
+    _caller: BaseListController
+
+    def __init__(self, caller: BaseListController) -> None:
+        self._caller = caller
+        super().__init__()
+        self._widget.submit_button.clicked.connect(self.execute_action)
 
     @abstractmethod
     def _get_widget_instance(self) -> BaseCRUDWidget:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def execute_action(self) -> None:
         raise NotImplementedError()
