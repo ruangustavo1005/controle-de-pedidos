0301dee76fc123b94737fbbcdcf8d6fd488b0b24
Ruan Gustavo Moretti <ruan.gustavo.moretti10@gmail.com>
2024-04-30 20:51:27 -0300

    test: icon in work bar

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import sys
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 
 import database
 from routes.menu.controller import MenuController
+from settings import FAV_ICON_FILE_NAME
 
 database.create_if_not_exists()
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon(FAV_ICON_FILE_NAME))
 
 menu_controller = MenuController()
 menu_controller.show()
