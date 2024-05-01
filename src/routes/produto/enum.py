49c2a615a929e16fe7eb14d64937a513a3fa4a11
Ruan Gustavo Moretti <ruan.gustavo.moretti10@gmail.com>
2024-05-01 10:44:46 -0300

    feat: add half bucket and KG

--- src/routes/produto/enum.py
+++ src/routes/produto/enum.py
@@ -2,5 +2,7 @@ from common.enum import BaseEnum
 
 
 class ProdutoUnidadeMedidaEnum(BaseEnum):
+    MEIO_BALDE = ("meio-balde", "Meio Balde")
     BALDE = ("balde", "Balde")
+    MEIO_KG = ("meio-kg", "Meio KG")
     KG = ("kg", "KG")
